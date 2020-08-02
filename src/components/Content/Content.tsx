2f5451f5c84c2ff1cae6a649d8ba05d4e910f0e3
serozhkohanna <serozhkohanna@gmail.com>
2020-08-02 17:14:13 +0300

    Connected to DB, get Data func.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Content.scss';
 import NavParams from "../NavParams/NavParams";
-import axios from 'axios';
+import { getData } from "../../services/request";
 
 const Content = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+	getData('http://localhost:5000/listingsAndReviews')
+	  .then(data => setData(data));
+  }, [])
+
+  const renderData = () => {
+    if (data.length > 0) {
+	 return data.map((item: any, i) => {
+	   return <div key={i}>
+		 <p>{item.name}</p>
+		</div>
+	  })
+	} else return <div>loading or absence</div>
+  }
 
   return <section className='content'>
 	<NavParams/>
 	<div className="content-list">
-
+	  {renderData()}
 	</div>
   </section>
 }
