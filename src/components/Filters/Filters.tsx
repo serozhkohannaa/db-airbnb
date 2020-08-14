ce086c4530aa6eb5822a762f9228753485770b80
serozhkohanna <serozhkohanna@gmail.com>
2020-08-15 02:22:21 +0300

    PRice range, fix current value.

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -7,7 +7,6 @@ import { FiltersInterface } from "../../constants/filters.interface";
 import { connect } from 'react-redux';
 
 interface Props {
-  priceValue?: HTMLInputElement | null;
   typeValue?: HTMLInputElement | null;
   policyValue?: HTMLInputElement | null;
   setFilter: Function;
@@ -16,17 +15,18 @@ interface Props {
   cancellation_policy: ParamsInterfaces;
 }
 
-const Filters: FC<Props> = ({priceValue, typeValue, policyValue, setFilter, priceRange, propertyTypes, cancellation_policy}) => {
+const Filters: FC<Props> = ({ typeValue, policyValue, setFilter, priceRange, propertyTypes, cancellation_policy}) => {
+  const [price, setPriceValue] = useState(2499);
   const [checkedValues, setChecked] = useState();
   const [checkedPolicy, setCancellation] = useState();
   const [isHighScored, setHighScored] = useState(false);
 
-  let filterParams: FiltersInterface = {};
+  let filterParams: FiltersInterface = {price, isHighScored};
 
   const handleSubmit = (e: FormEvent) => {
 	e.preventDefault();
 
-	filterParams.price = priceValue?.value;
+	filterParams.price = price;
 	filterParams.property_type = checkedValues?.length > 0 ? checkedValues : propertyTypes;
 	filterParams.cancellation_policy = checkedPolicy?.length > 0 ? checkedPolicy : cancellation_policy;
 	filterParams.isHighScored = isHighScored;
@@ -78,18 +78,23 @@ const Filters: FC<Props> = ({priceValue, typeValue, policyValue, setFilter, pric
 	setHighScored(!isHighScored);
   }
 
+  const handleValueChange = (e) => {
+	setPriceValue(e.target.value);
+  }
+
   return <form className='filters' onSubmit={handleSubmit}>
 	<div className="option option-price">
 	  <div className="title">
 		Price
 	  </div>
 	  <div className="input">
-		<input ref={(input) => priceValue = input} type="range" min={priceRange.min} max={priceRange.max} step={10}
+		<input onChange={handleValueChange} value={price} type="range" min={priceRange.min} max={priceRange.max} step={10}
 			   list="tickmarks"/>
 		<datalist id="tickmarks" className='data-list'>
 		  <option value={priceRange.min} label={priceRange.min?.toString()}>{priceRange.min}</option>
 		  <option value={priceRange.max} label={priceRange.max?.toString()}>{priceRange.max}</option>
 		</datalist>
+		<input type="text" className='input-value' value={price} disabled/>
 	  </div>
 	</div>
 	<div className="option option-type">
