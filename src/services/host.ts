41e7de40e88c0664187acd90b9eeffbb00572904
Serozhko, Hanna <serozhkohanna@gmail.com>
2020-09-28 12:36:38 +0300

    make it better

--- src/services/host.ts
+++ src/services/host.ts
@@ -1,2 +1,2 @@
-export const DEPLOY_URL = 'https://db-airbnb-demo.herokuapp.com/listingsAndReviews';
-// export const DEPLOY_URL = 'http://localhost:5000/listingsAndReviews';
+// export const DEPLOY_URL = 'https://db-airbnb-demo.herokuapp.com/listingsAndReviews';
+export const DEPLOY_URL = 'http://localhost:5000/listingsAndReviews';
